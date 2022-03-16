da496bfc61f58f1bd157675880391885bdfc7b37
Taylor Rusthoven <92177706+BluejayTay@users.noreply.github.com>
2022-03-16 18:28:27 -0500

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,10 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 //import Game from "./Game";
+//import GameForm from "./GameForm";
 import Welcome from "./Welcome";
 import Nav from "./Nav";
+//import StudyListIndex from "./StudyListIndex";
 
 function App() {
   return (
@@ -10,7 +12,9 @@ function App() {
       <Router>
         <Nav />
         <Switch>
-          <Route exact path="/" render={() => <Welcome />} />
+          <Route exact path="/">
+            <Welcome />
+          </Route>
         </Switch>
       </Router>
     </div>
