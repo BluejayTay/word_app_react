d904492d89f78cdcd0fdc3abf99b4e80413794d6
Taylor Rusthoven <92177706+BluejayTay@users.noreply.github.com>
2022-03-16 18:26:15 -0500

    Create useApiLists.js

---
+++ src/components/useApiLists.js
@@ -0,0 +1,21 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+export default function useApiLists() {
+  const [apiLists, setApiLists] = useState([]);
+  const [status, setStatus] = useState("unloaded");
+
+  useEffect(() => {
+    requestApiLists();
+
+    async function requestApiLists() {
+      setStatus("loading");
+      axios.get(`http://localhost:3000/api/study_lists`).then((response) => {
+        setApiLists(response.data);
+      });
+      setStatus("loaded");
+    }
+  }, []);
+
+  return [apiLists, status];
+}
