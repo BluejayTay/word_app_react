96b0e20fbdc7fbb72cfb6cf28da67553143b2012
Taylor Rusthoven <92177706+BluejayTay@users.noreply.github.com>
2022-03-18 16:24:35 -0500

    Update GameForm.js
    fix code to send studyListId as id params to game route

--- src/components/GameForm.js
+++ src/components/GameForm.js
@@ -1,29 +1,44 @@
 import { useState } from "react";
-//import { Redirect } from "react-router-dom";
+import { Redirect } from "react-router-dom";
 import useApiLists from "./useApiLists";
+//import Game from "./Game";
 
 const GameForm = () => {
   const [studyLists] = useApiLists();
-  const [studyList, setStudyList] = useState("");
+  const [studyListId, setStudyListId] = useState();
+  const [toGame, setToGame] = useState(false);
+
+  if (toGame == true) {
+    return (
+      <Redirect
+        to={{
+          pathname: `/study_lists/${studyListId}/game`,
+        }}
+      />
+    );
+  }
 
   return (
     <div>
       <form
         onSubmit={(e) => {
           e.preventDefault();
+          setToGame(true);
         }}
       >
         <label htmlFor="studyList">
-          Choose a List:
+          Chose a list:
           <select
             id="studyList"
-            value={studyList}
-            onChange={(e) => setStudyList(e.target.value)}
-            onBlur={(e) => setStudyList(e.target.value)}
+            //value={studyList}
+            onChange={(e) => setStudyListId(e.target.value)}
+            onBlur={(e) => setStudyListId(e.target.value)}
           >
             <option />
-            {studyLists.map((studyList) => (
-              <option key={studyList.id}>{studyList.title}</option>
+            {studyLists.map((List) => (
+              <option key={List.id} value={List.id}>
+                {List.title}
+              </option>
             ))}
           </select>
         </label>
