6b39b563c0999fb01f68ed23813a8ea235ddd907
Taylor Rusthoven <92177706+BluejayTay@users.noreply.github.com>
2022-03-22 20:16:00 -0500

    makes synonym draggable

--- src/components/Synonym.js
+++ src/components/Synonym.js
@@ -1,8 +1,31 @@
+import { useState } from "react";
+import { useDrag } from "react-dnd";
+
 const Synonym = ({ synonym, matchIndex }) => {
   const name = synonym.name;
+  //const id = synonym.id;
+  const [selected, setSelected] = useState(false);
+  const [{ isDragging }, dragRef] = useDrag({
+    type: "synonym",
+    item: {
+      name: name,
+    },
+    collect: (monitor) => ({
+      isDragging: monitor.isDragging(),
+    }),
+  });
+
   return (
-    <div style={{ display: "flex" }}>
-      <div style={{ background: "pink" }}>{matchIndex}</div>
+    <div style={{ display: "flex" }} ref={dragRef}>
+      {isDragging}
+      <button
+        id={matchIndex}
+        value={matchIndex}
+        style={{ background: selected ? "blue" : "pink" }}
+        onClick={() => (!selected ? setSelected(true) : setSelected(false))}
+      >
+        {matchIndex}
+      </button>
       <div style={{ background: "orange" }}>
         <h1>{name}</h1>
       </div>
