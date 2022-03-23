68d42eb9e88fa0821a475917e4fde59ad64e3127
Taylor Rusthoven <92177706+BluejayTay@users.noreply.github.com>
2022-03-22 20:19:15 -0500

    Update Word.js
    adds temporary button to display selection and matchIndex

--- src/components/Word.js
+++ src/components/Word.js
@@ -1,13 +1,23 @@
-//import { Link } from "react-router-dom";
+import { useState } from "react";
 
 const Word = ({ word, matchIndex }) => {
   const name = word.name;
+  const [selected, setSelected] = useState(false);
+
   return (
-    <div style={{ display: "flex" }}>
+    <div style={{ display: "flex", justifyContent: "end" }}>
       <div style={{ background: "orange" }}>
         <h1>{name}</h1>
       </div>
-      <div style={{ background: "pink" }}>{matchIndex}</div>
+      <button
+        ref={word.matchIndex}
+        id={matchIndex}
+        value={matchIndex}
+        style={{ background: selected ? "blue" : "pink" }}
+        onClick={() => (!selected ? setSelected(true) : setSelected(false))}
+      >
+        {matchIndex}
+      </button>
     </div>
   );
 };
