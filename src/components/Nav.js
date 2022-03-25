716a2a8f36c377f43e3c1e1649a4d2d353c3b371
Taylor Rusthoven <92177706+BluejayTay@users.noreply.github.com>
2022-03-25 15:20:35 -0500

    Update Nav.js

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 
 const Nav = () => {
   return (
-    <nav>
+    <nav style={{ backgroundColor: "silver" }}>
       <Link to="/">WerdNerd</Link>
     </nav>
   );
