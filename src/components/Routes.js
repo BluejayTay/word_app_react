f010be58774bd7e4047b8cfcef472abc4c96129a
Taylor Rusthoven <92177706+BluejayTay@users.noreply.github.com>
2022-03-25 17:30:23 -0500

    adds route for StudyListForm

--- src/components/Routes.js
+++ src/components/Routes.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Nav from "./Nav";
 import Game from "./Game";
 import Welcome from "./Welcome";
+import StudyListForm from "./StudyListForm";
 
 const Routes = () => {
   return (
@@ -15,6 +16,11 @@ const Routes = () => {
             exact
             path="/study_lists/:study_list_id/game"
             render={(props) => <Game {...props} />}
+          />
+          <Route
+            exact
+            path="/study_lists/new"
+            render={() => <StudyListForm />}
           />
         </Switch>
       </Router>
