19789ca981dbf1be1cb046a8e499839df94e9c9f
Taylor Rusthoven <92177706+BluejayTay@users.noreply.github.com>
2022-03-25 17:31:20 -0500

    adds link for StudyListForm to welcome page

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,6 +1,6 @@
 import React from "react";
 import GameForm from "./GameForm";
-//import { Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 //import StudyListIndex from "./StudyListIndex";
 
 const Welcome = () => {
@@ -9,6 +9,9 @@ const Welcome = () => {
       <h1>Welcome to WerdNerd</h1>
       <h2>Play the WerdWeb game with one of our word lists now!</h2>
       <GameForm />
+      <div>
+        <Link to={`/study_lists/new`}>make a new word list!</Link>
+      </div>
     </div>
   );
 };
