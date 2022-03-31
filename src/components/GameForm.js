7acf0ab6e01cba3fe638bb75f5cbec62485b121e
Taylor Rusthoven <92177706+BluejayTay@users.noreply.github.com>
2022-03-30 20:22:11 -0500

    moves study_lists axios request to GameForm.js

--- src/components/GameForm.js
+++ src/components/GameForm.js
@@ -1,12 +1,23 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Redirect } from "react-router-dom";
-import useApiLists from "./useApiLists";
+//import useApiLists from "./useApiLists";
+import axios from "axios";
 //import Game from "./Game";
 
-const GameForm = () => {
-  const [studyLists] = useApiLists();
+const GameForm = ({ user }) => {
+  const userId = user["id"];
+  const [studyLists, setStudyLists] = useState([]);
   const [studyListId, setStudyListId] = useState();
   const [toGame, setToGame] = useState(false);
+
+  useEffect(() => {
+    axios
+      .get(`http://localhost:3000/api/study_lists`, { user_id: userId })
+      .then((response) => {
+        setStudyLists(response.data);
+        console.log(response.data);
+      });
+  }, [userId]);
 
   if (toGame == true) {
     return (
@@ -30,7 +41,6 @@ const GameForm = () => {
           Chose a list:
           <select
             id="studyList"
-            //value={studyList}
             onChange={(event) => setStudyListId(event.target.value)}
             onBlur={(event) => setStudyListId(event.target.value)}
           >
