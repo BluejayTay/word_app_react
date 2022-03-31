71393db3f71650affff476ef0eab751de5ecb765
Taylor Rusthoven <92177706+BluejayTay@users.noreply.github.com>
2022-03-30 20:29:14 -0500

    Update Welcome.js
    adds sign-in link to welcome.js, removes extraneous imports

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,16 +1,19 @@
-import React from "react";
 import GameForm from "./GameForm";
 import { Link } from "react-router-dom";
-//import StudyListIndex from "./StudyListIndex";
 
-const Welcome = () => {
+const Welcome = ({ user }) => {
+  const guest = "fellow nerd";
+
   return (
     <div>
-      <h1>Welcome to WerdNerd</h1>
+      <h1>Welcome to WerdNerd, {user["email"] || guest}!</h1>
       <h2>Play the WerdWeb game with one of our word lists now!</h2>
-      <GameForm />
+      <GameForm user={user} />
       <div>
         <Link to={`/study_lists/new`}>make a new word list!</Link>
+      </div>
+      <div>
+        <Link to={"users/login"}>Sign-up/Log-in</Link>
       </div>
     </div>
   );
