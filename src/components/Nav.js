e9ffeccc4b362094205828b4bac84b720279675a
Taylor Rusthoven <92177706+BluejayTay@users.noreply.github.com>
2022-04-05 20:23:20 -0500

    styles nav component

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,16 +1,14 @@
-import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 //import "./Nav.css";
 
-const Nav = ({ user, setUser }) => {
-  const [message, setMessage] = useState("");
-
+const Nav = ({ setUser }) => {
   const renderUserButtons = () => {
     const token = localStorage.getItem("auth_token");
     if (token)
       return (
         <Link
-          to="/"
+          to={"#"}
+          className="btn btn-logout"
           onClick={() => {
             localStorage.clear();
             setUser({});
@@ -19,19 +17,31 @@ const Nav = ({ user, setUser }) => {
           Log out
         </Link>
       );
-    else return <Link to={"/users/login"}>Sign-up/Log-in</Link>;
+    else
+      return (
+        <Link className="btn btn-login" to={"/users/login"}>
+          Log in/Sign up
+        </Link>
+      );
   };
 
-  useEffect(() => {
-    const token = localStorage.getItem("auth_token");
-    token ? setMessage(`Current user: ${user["email"]}`) : setMessage("");
-  }, [user]);
-
   return (
-    <nav style={{ backgroundColor: "silver" }}>
-      <Link to="/">WerdNerd </Link>
-      <span>{message}</span>
-      {renderUserButtons()}
+    <nav className="navbar p-0">
+      <div className="container-fluid ">
+        <Link className="navbar-brand" to={"/"}>
+          WerdNerd
+        </Link>
+        <div className="navbar-nav">
+          <ul className="nav d-flex align-items-center">
+            <li className="nav-item">
+              <Link to={"/"} className="btn btn-home me-3">
+                <i className="bi bi-house-fill"></i>
+              </Link>
+            </li>
+            <li className="nav-item">{renderUserButtons()}</li>
+          </ul>
+        </div>
+      </div>
     </nav>
   );
 };
