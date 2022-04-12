42b4aad0f016d0ef336ab3dcef351ad429d494d7
Taylor Rusthoven <92177706+BluejayTay@users.noreply.github.com>
2022-04-11 21:23:20 -0500

    adds styled component to Nav.js

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,5 +1,46 @@
 import { Link } from "react-router-dom";
 //import "./Nav.css";
+import styled from "styled-components";
+
+const StyledNav = styled.nav`
+  .navbar-brand {
+    font-family: "Rampart One", sans-serif;
+    font-size: 32px;
+    color: #ee500c;
+  }
+  a:hover.navbar-brand {
+    text-decoration: none;
+    color: #ff621f;
+  }
+  .navbar {
+    font-family: "Inter", sans-serif;
+    background-color: #fcfdf1;
+  }
+  .btn-login,
+  .btn-logout {
+    background-color: #ff621f;
+    color: #e7fb9d;
+    font-weight: 600;
+    font-size: 20px;
+    text-decoration: none;
+  }
+  .btn-login:hover {
+    background-color: #e7fb9d;
+    color: #ff621f;
+  }
+  .btn-home {
+    background-color: #8ae7f5;
+    color: #ff621f;
+    text-decoration: none;
+  }
+  .btn-home:hover {
+    background-color: #ff621f;
+    color: #8ae7f5;
+  }
+  .bi-house-fill {
+    font-size: 20px;
+  }
+`;
 
 const Nav = ({ setUser }) => {
   const renderUserButtons = () => {
@@ -26,23 +67,25 @@ const Nav = ({ setUser }) => {
   };
 
   return (
-    <nav className="navbar p-0">
-      <div className="container-fluid ">
-        <Link className="navbar-brand" to={"/"}>
-          WerdNerd
-        </Link>
-        <div className="navbar-nav">
-          <ul className="nav d-flex align-items-center">
-            <li className="nav-item">
-              <Link to={"/"} className="btn btn-home me-3">
-                <i className="bi bi-house-fill"></i>
-              </Link>
-            </li>
-            <li className="nav-item">{renderUserButtons()}</li>
-          </ul>
+    <StyledNav>
+      <nav className="navbar p-0">
+        <div className="container-fluid ">
+          <Link className="navbar-brand" to={"/"}>
+            WerdNerd
+          </Link>
+          <div className="navbar-nav">
+            <ul className="nav d-flex align-items-center">
+              <li className="nav-item">
+                <Link to={"/"} className="btn btn-home me-3">
+                  <i className="bi bi-house-fill"></i>
+                </Link>
+              </li>
+              <li className="nav-item">{renderUserButtons()}</li>
+            </ul>
+          </div>
         </div>
-      </div>
-    </nav>
+      </nav>
+    </StyledNav>
   );
 };
 
