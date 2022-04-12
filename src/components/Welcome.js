a7a41343ab281121fa814919955365eb7ce6d4ef
Taylor Rusthoven <92177706+BluejayTay@users.noreply.github.com>
2022-04-11 21:28:11 -0500

    adds styled component to Welcome.js

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -2,6 +2,13 @@ import { useState } from "react";
 import { Link } from "react-router-dom";
 import ErrorMessage from "./ErrorMessage";
 import GameForm from "./GameForm";
+import styled from "styled-components";
+
+const StyledWelcome = styled.div`
+  .welcome-container {
+    background-color: #e6fdff;
+  }
+`;
 
 const Welcome = ({ user }) => {
   const [error, setError] = useState("");
@@ -9,22 +16,23 @@ const Welcome = ({ user }) => {
   return (
     <div>
       <ErrorMessage error={error} setError={setError} />
-      <div className="container">
-        <h1 className="text-center mt-5">
-          Welcome, {user["email"] || `fellow nerd`}!
-        </h1>
+      <div className="container d-flex justify-content-center">
+        <StyledWelcome>
+          <div className="welcome-container card p-5 my-5 justify-content-center">
+            <h1 className="text-center mt-5">
+              Welcome, {user["email"] || `fellow nerd`}!
+            </h1>
 
-        <div className="form text-center mt-5">
-          <h2>
-            Play <span className="brand-style">WerdWeb</span> now!
-          </h2>
-          <GameForm user={user} error={error} setError={setError} />
-        </div>
-        <div>
-          <Link to={`/study_lists/new`} className="btn btn-new">
-            Make a new word list!
-          </Link>
-        </div>
+            <div className="text-center mt-5">
+              <GameForm user={user} error={error} setError={setError} />
+            </div>
+            <div className="text-center mt-4">
+              <Link to={`/study_lists/new`} className="btn btn-new">
+                Make a new word list
+              </Link>
+            </div>
+          </div>
+        </StyledWelcome>
       </div>
     </div>
   );
