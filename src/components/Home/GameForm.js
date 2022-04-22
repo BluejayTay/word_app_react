17e5ec3d0b45f08d658ec272f9321f9be33973ee
Taylor Rusthoven <92177706+BluejayTay@users.noreply.github.com>
2022-04-22 11:15:10 -0500

    moves axios back to GameForm

--- src/components/Home/GameForm.js
+++ src/components/Home/GameForm.js
@@ -1,10 +1,44 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Redirect } from "react-router-dom";
+import { API_ROOT } from "../../apiRoot";
+import axios from "axios";
 
-const GameForm = ({ studyLists, setError }) => {
+const GameForm = ({ user, setLoading, setError }) => {
   const defaultOption = "Choose a list";
+  const [studyLists, setStudyLists] = useState([]);
   const [studyListId, setStudyListId] = useState();
   const [toGame, setToGame] = useState(false);
+
+  useEffect(() => {
+    const token = sessionStorage.getItem("auth_token");
+    if (token)
+      axios
+        .get(`${API_ROOT}api/study_lists`, {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+          user_id: user["id"],
+        })
+        .then((response) => {
+          console.log(response.data);
+          setStudyLists(response.data);
+          setLoading(false);
+        })
+        .catch((error) => {
+          console.log(error);
+          requestBaseLists();
+        });
+    else requestBaseLists();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [user]);
+
+  async function requestBaseLists() {
+    axios.get(`${API_ROOT}api/study_lists`).then((response) => {
+      console.log(response.data);
+      setStudyLists(response.data);
+      setLoading(false);
+    });
+  }
 
   const handleGame = () => {
     studyListId && studyListId != defaultOption
